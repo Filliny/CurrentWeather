c1bd9a0706d561c3185fd7b9e7ea2a198e29bc71
Oleg Filipchuk <oleg@timtyler.org.ua>
2021-07-04 14:38:02 +0300

    fix intendation in hourly

--- script/weather.js
+++ script/weather.js
@@ -4,16 +4,15 @@ let hourlyForecast = {};
 let apiKey = "1948c1197ccfc6803638b11bae5c4d3c";
 
 
-
-function fillFromOrigin(){ //we need here to get user position from another api service...
+function fillFromOrigin() { //we need here to get user position from another api service...
 
     let city;
     setCustomValidity();
     readCookie();
 
-    if("weather_fav_city" in cookieStorage){
+    if ("weather_fav_city" in cookieStorage) {
         city = cookieStorage["weather_fav_city"];
-    }else{
+    } else {
         city = "Киев";
     }
 
@@ -22,39 +21,34 @@ function fillFromOrigin(){ //we need here to get user position from another api 
 
 }
 
-
-
-
-
-function fillFromSearch(){
-    let city= document.getElementsByTagName('input')[0].value;
+function fillFromSearch() {
+    let city = document.getElementsByTagName('input')[0].value;
     getCityDayForecast(city);
     fillCurrentDay();
 }
 
-function getCityDayForecast(city){
+function getCityDayForecast(city) {
 
 
     let request;
-    if(window.XMLHttpRequest){
+    if (window.XMLHttpRequest) {
         request = new XMLHttpRequest();
-    }
-    else{
+    } else {
         request = new ActiveXObject("Microsoft.XMLHTTP");
     }
-        let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-    request.open("GET",apiURL);
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    request.open("GET", apiURL);
 
-    request.onerror = function (){
+    request.onerror = function () {
         console.log(request.errorCode)
     }
-    request.onload = function (){
-        if(request.status === 200){
+    request.onload = function () {
+        if (request.status === 200) {
             dayForecast = JSON.parse(request.response);
             console.log(dayForecast)
             fillCurrentDay();
             setCookie(city);
-        }else{
+        } else {
             showError();
         }
     }
@@ -62,24 +56,23 @@ function getCityDayForecast(city){
 }
 
 
-function getCityHourlyForecast(city){
+function getCityHourlyForecast(city) {
 
 
     let requestHourly;
-    if(window.XMLHttpRequest){
+    if (window.XMLHttpRequest) {
         requestHourly = new XMLHttpRequest();
-    }
-    else{
+    } else {
         requestHourly = new ActiveXObject("Microsoft.XMLHTTP");
     }
     let apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&cnt=6&appid=${apiKey}`;
-    requestHourly.open("GET",apiURL);
+    requestHourly.open("GET", apiURL);
 
-    requestHourly.onerror = function (){
+    requestHourly.onerror = function () {
         console.log(requestHourly.errorCode)
     }
-    requestHourly.onload = function (){
-        if(requestHourly.status === 200){
+    requestHourly.onload = function () {
+        if (requestHourly.status === 200) {
             hourlyForecast = JSON.parse(requestHourly.response);
             console.log(hourlyForecast)
 
@@ -93,33 +86,33 @@ function getCityHourlyForecast(city){
 
 
 //handle enter on input
-document.querySelector('input').onkeypress = function(e){
-    if(!e.target.reportValidity())
+document.querySelector('input').onkeypress = function (e) {
+    if (!e.target.reportValidity())
         return;
     //
     if (!e) e = window.event;
     let keyCode = e.code || e.key;
-    if (keyCode === 'Enter'){
+    if (keyCode === 'Enter') {
         fillFromSearch()
         return false;
     }
 }
 
-function setCustomValidity(){
+function setCustomValidity() {
 
     let searchInput = document.querySelector('input');
 
-    searchInput.addEventListener("invalid",function (){
-        searchInput.setCustomValidity(searchInput.validity.valid ? '' :'Please, enter city name.');
+    searchInput.addEventListener("invalid", function () {
+        searchInput.setCustomValidity(searchInput.validity.valid ? '' : 'Please, enter city name.');
     })
 
-    searchInput.addEventListener("input",function (){
+    searchInput.addEventListener("input", function () {
         searchInput.setCustomValidity('');
     })
 }
 
 
-function fillCurrentDay(){
+function fillCurrentDay() {
 
     hideError();
 
@@ -134,59 +127,60 @@ function fillCurrentDay(){
 
     cityElement.innerText = dayForecast.name;//["name"];
     weatherElement.innerText = dayForecast.weather[0].main;
-    imageElement.src= `https://openweathermap.org/img/wn/${ dayForecast.weather[0].icon}@2x.png`
-    currTempElement.innerText = Math.floor(dayForecast.main.temp)+"ºC";
-    minTempElement.innerText = Math.floor(dayForecast.main.temp_min)+"ºC";
-    maxTempElement.innerText = Math.floor(dayForecast.main.temp_max)+"ºC";
+    imageElement.src = `https://openweathermap.org/img/wn/${dayForecast.weather[0].icon}@2x.png`
+    currTempElement.innerText = Math.floor(dayForecast.main.temp) + "ºC";
+    minTempElement.innerText = Math.floor(dayForecast.main.temp_min) + "ºC";
+    maxTempElement.innerText = Math.floor(dayForecast.main.temp_max) + "ºC";
     windSpeedElement.innerText = (dayForecast.wind.speed * 3.6).toFixed(1).toString();
     dateElement.innerText = new Date().toLocaleDateString('ru-RU');
 
 }
 
-function fillHourly(){
+function fillHourly() {
     const parent = document.getElementById("hourly_weather");
 
     let weatherArr = hourlyForecast.list;
 
-    let weekDayElement =  document.getElementById('week_day');
-    let date = (weatherArr[0].dt_txt).replace(' ','T');
+    let weekDayElement = document.getElementById('week_day');
+    let date = (weatherArr[0].dt_txt).replace(' ', 'T');
     let newDate = new Date(date);
-    weekDayElement.innerText = newDate.toLocaleDateString("en-US", { weekday: 'long' });
+    weekDayElement.innerText = newDate.toLocaleDateString("en-US", {weekday: 'long'});
 
-    for(let i = 0; i < weatherArr.length; i++){
+    for (let i = 0; i < weatherArr.length; i++) {
 
+        let start = 4;
 
         let hourElement = document.createElement('div');
-        hourElement.id = "hour"+i;
+        hourElement.id = "hour" + i;
         hourElement.className = "hourly";
         hourElement.style.gridRow = "3";
-        hourElement.style.gridColumn = (3+i).toString();
-        let date = (weatherArr[i].dt_txt).replace(' ','T');
+        hourElement.style.gridColumn = (start + i).toString();
+        let date = (weatherArr[i].dt_txt).replace(' ', 'T');
         let newDate = new Date(date);
-        hourElement.innerText = newDate.getHours()+":00";
+        hourElement.innerText = newDate.getHours() + ":00";
 
         let imgElement = document.createElement('img');
-        imgElement.src = `https://openweathermap.org/img/wn/${ weatherArr[i].weather[0].icon}@2x.png`
+        imgElement.src = `https://openweathermap.org/img/wn/${weatherArr[i].weather[0].icon}@2x.png`
         imgElement.style.gridRow = "4";
-        imgElement.style.gridColumn = (3+i).toString();
+        imgElement.style.gridColumn = (start + i).toString();
 
         let forecastElement = document.createElement('div');
         forecastElement.className = "hourly";
         forecastElement.style.gridRow = "6";
-        forecastElement.style.gridColumn = (3+i).toString();
+        forecastElement.style.gridColumn = (start + i).toString();
         forecastElement.innerText = weatherArr[i].weather[0].main;
 
         let tempElement = document.createElement('div');
         tempElement.className = "hourly";
         tempElement.style.gridRow = "8";
-        tempElement.style.gridColumn = (3+i).toString();
-        tempElement.innerText = (weatherArr[i].main.temp).toFixed(1)+"ºC";
+        tempElement.style.gridColumn = (start + i).toString();
+        tempElement.innerText = (weatherArr[i].main.temp).toFixed(1) + "ºC";
 
         let windElement = document.createElement('div');
         windElement.className = "hourly";
         windElement.style.gridRow = "10";
-        windElement.style.gridColumn = (3+i).toString();
-        windElement.innerText = (weatherArr[i].wind.speed*3.6).toFixed(1).toString();
+        windElement.style.gridColumn = (start + i).toString();
+        windElement.innerText = (weatherArr[i].wind.speed * 3.6).toFixed(1).toString();
 
         parent.appendChild(windElement)
         parent.appendChild(tempElement);
@@ -194,37 +188,33 @@ function fillHourly(){
         parent.append(imgElement);
         parent.append(hourElement);
 
-
-
-
-
     }
 }
 
 
-function setCookie(city){
+function setCookie(city) {
     let expDate = new Date();
-    expDate.setDate(expDate.getDate()+1);
-    document.cookie = "weather_fav_city="+city+";expires="+expDate.toGMTString()+";path/";
+    expDate.setDate(expDate.getDate() + 1);
+    document.cookie = "weather_fav_city=" + city + ";expires=" + expDate.toGMTString() + ";path/";
 }
 
 
-function readCookie(){
+function readCookie() {
     let strings = document.cookie.split('; ');
-    for(let i=0; i < strings.length; i++){
+    for (let i = 0; i < strings.length; i++) {
         let data = strings[i].split('=');
         cookieStorage[data[0]] = data[1];
     }
 
 }
 
-function showError(){
+function showError() {
     document.getElementById('current_weather').style.display = "none";
     document.getElementById('error_weather').style.display = "grid";
     document.getElementById('hourly_weather').style.display = "none";
 }
 
-function hideError(){
+function hideError() {
     document.getElementById('current_weather').style.display = "grid";
     document.getElementById('hourly_weather').style.display = "grid";
     document.getElementById('error_weather').style.display = "none";
